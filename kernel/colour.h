efdd010d732ecefee4589a9eb7797c810c253ef4
Claudio Gheller <cgheller@ela3.(none)>
2015-11-01 06:26:15 +0100

    New Splotch version

--- kernel/colour.h
+++ kernel/colour.h
@@ -23,43 +23,7 @@
 #ifndef RAYPP_COLOUR_H
 #define RAYPP_COLOUR_H
 
-#include "cxxsupport/datatypes.h"
-
-template<typename T> class RGB_tuple
-  {
-  public:
-    T r, g, b;
-
-    RGB_tuple () {}
-    RGB_tuple (T rv, T gv, T bv)
-      : r (rv), g (gv), b (bv) {}
-    template<typename T2> explicit RGB_tuple (const RGB_tuple<T2> &orig)
-      : r(orig.r), g(orig.g), b(orig.b) {}
-
-    const RGB_tuple &operator= (const RGB_tuple &Col2)
-      { r=Col2.r; g=Col2.g; b=Col2.b; return *this; }
-    const RGB_tuple &operator+= (const RGB_tuple &Col2)
-      { r+=Col2.r; g+=Col2.g; b+=Col2.b; return *this; }
-    const RGB_tuple &operator*= (T fac)
-      { r*=fac; g*=fac; b*=fac; return *this; }
-    RGB_tuple operator+ (const RGB_tuple &Col2) const
-      { return RGB_tuple (r+Col2.r, g+Col2.g, b+Col2.b); }
-    RGB_tuple operator- (const RGB_tuple &Col2) const
-      { return RGB_tuple (r-Col2.r, g-Col2.g, b-Col2.b); }
-    template<typename T2> RGB_tuple operator* (T2 factor) const
-      { return RGB_tuple (r*factor, g*factor, b*factor); }
-    template<typename T2> friend inline RGB_tuple operator* (T2 factor, const RGB_tuple &Col)
-      { return RGB_tuple (Col.r*factor, Col.g*factor, Col.b*factor); }
-
-    void Set (T r2, T g2, T b2)
-      { r=r2; g=g2; b=b2; }
-
-    friend std::ostream &operator<< (std::ostream &os, const RGB_tuple &c)
-      {
-      os << "(" << c.r << ", " << c.g << ", " << c.b << ")";
-      return os;
-      }
-  };
+#include "cxxsupport/colour.h"
 
 typedef RGB_tuple<float> COLOUR;
 
--- kernel/colourmap.h
+++ kernel/colourmap.h
@@ -23,61 +23,9 @@
 #ifndef RAYPP_COLOURMAP_H
 #define RAYPP_COLOURMAP_H
 
-#include <vector>
 #include "kernel/colour.h"
-#include "cxxsupport/sort_utils.h"
-#include "cxxsupport/math_utils.h"
+#include "cxxsupport/linear_map.h"
 
-template<typename T> class anythingMap
-  {
-  private:
-    bool sorted;
-    std::vector<double> x;
-    std::vector<T> y;
-
-  public:
-    void addVal (double x_, const T &val)
-      {
-      sorted=false;
-      x.push_back(x_);
-      y.push_back(val);
-      }
-
-    void sortMap()
-      {
-      std::vector<size_t> idx;
-      buildIndex(x.begin(),x.end(),idx);
-      sortByIndex(x.begin(),x.end(),idx);
-      sortByIndex(y.begin(),y.end(),idx);
-      sorted = true;
-      }
-
-    T getVal (double x_)
-      {
-      planck_assert(x.size()>0,"trying to access an empty map");
-      if (x.size()==1) return y[0];
-      if (!sorted) sortMap();
-      tsize index;
-      double frac;
-      interpol_helper (x.begin(), x.end(), x_, index, frac);
-      return (1.-frac)*y[index]+frac*y[index+1];
-      }
-    T getVal_const (double x_) const
-      {
-      planck_assert(x.size()>0,"trying to access an empty map");
-      planck_assert(sorted,"map must be sorted");
-      if (x.size()==1) return y[0];
-      tsize index;
-      double frac;
-      interpol_helper (x.begin(), x.end(), x_, index, frac);
-      return (1.-frac)*y[index]+frac*y[index+1];
-      }
-
-    size_t size() const { return x.size(); }
-    double getX (size_t idx) { if (!sorted) sortMap(); return x[idx]; }
-    T getY (size_t idx) { if (!sorted) sortMap(); return y[idx]; }
-  };
-
-typedef anythingMap<COLOUR> COLOURMAP;
+typedef linearMap<COLOUR> COLOURMAP;
 
 #endif
